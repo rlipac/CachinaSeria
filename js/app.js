4f0ab7f06ed81c6ed7594e95065dfc7bd468058f
Richard Lipa <rlipac31@gmail.com>
2017-11-30 17:27:22 -0500

    opt1

--- js/app.js
+++ js/app.js
@@ -11,3 +11,38 @@ $offCanvas.find('li').click(function (ev){
 $offCanvas.foundation('close') 
     
 })
+
+<!--   boton veta boleto -->
+  
+  
+class Toggable {
+    constructor (el) {
+        // inicializar el estado  interno
+        
+        this.el = el
+        this.el.innerHTML = 'Comprar Boleto'
+        this.activated = false
+        this.el.addEventListener('click', this.onClick.bind(this))
+        
+    }
+    
+    onClick(ev) {
+       // acciona el botonn
+        this.activated = !this.activated
+        this.toggleText()
+        
+    }
+    
+    toggleText ()  {
+        // cambia el texto on : off
+    this.el.innerHTML = this.activated ? 'Vendido' : 'Comprar Boleto'
+        
+    }
+    
+}
+
+const button = document.getElementById('boton')// captura el valor del Id
+const miBoton = new Toggable(button)// otorga nuevo valor del Id
+
+
+<!--   boton veta boleto -->
